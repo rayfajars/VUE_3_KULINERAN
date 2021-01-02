7e84c7133325748efeb1364f4809ac9fdcc0968b
rayfajars <rayfajars@gmail.com>
2021-01-03 01:57:09 +0700

    add pesanan sukses

--- src/views/PesananSukses.vue
+++ src/views/PesananSukses.vue
@@ -4,8 +4,10 @@
       <div class="container">
           <div class="row justify-content-center">
               <div class="col text-center">
+                  <img src="assets/images/menunggu.png"  width="300" class="mb-3">
                   <h2><strong>Sukses dipesan!</strong></h2>
-                  <h4>Pesanan Anda Segera diproses <br> Terima Kasih</h4>
+                  <br>
+                  <h4>Pesanan Anda Segera diproses <br> <br> Terima Kasih</h4>
               </div>
           </div>
       </div>
