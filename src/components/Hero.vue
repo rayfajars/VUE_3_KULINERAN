cf11808a033ffab54bb776f91f7269a582b8352a
rayfajars <rayfajars@gmail.com>
2021-01-02 20:55:12 +0700

    create home, add images

---
+++ src/components/Hero.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <div class="d-flex h-100"> 
+                <div class="justify-content-center align-self-center">
+                    <h2><strong>Menu Makanan Enak,</strong> <br> di Gadget Mu !</h2>
+                    <p>Ayo segera pilih makanan favorit mu !</p>
+                    <button class="btn btn-lg btn-success">Pesan <b-icon-caret-right></b-icon-caret-right></button>
+                </div>
+            </div>
+           
+        </div>
+        <div class="col">
+            <div class="d-flex h-100">
+                <div class="justify-content-center align-self-center">
+                    <img src="../assets/images/hero.png" alt="" width="100%">
+                </div>
+            </div>
+            
+
+        </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name:"Hero"
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Foods.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar />
+    <h2>Daftar makanan</h2>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Foods",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+</style>
